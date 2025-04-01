b2ec0edea027994d9ccfcfea66dc3c469d3ccd5f
Mozin <temiumaru@naver.com>
2025-04-01 15:46:49 +0900

    키 입력

--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -10,19 +10,11 @@ public class Testing : MonoBehaviour
         if (Input.anyKey)
             Debug.Log("키 누르는 중");
 
-        if (Input.GetKeyDown(KeyCode.Return))
-            Debug.Log("엔터 누름");
-        if (Input.GetKey(KeyCode.LeftArrow))
-            Debug.Log("왼쪽 입력중");
-        if (Input.GetKeyUp(KeyCode.RightArrow))
-            Debug.Log("오른쪽 뗌");
-        
-        
-        if (Input.GetMouseButtonDown(0))
-            Debug.Log("마우스 입력");
-        if (Input.GetMouseButton(0))
-            Debug.Log("마우스 누르는중");
-        if (Input.GetMouseButtonUp(0))
-            Debug.Log("마우스 뗌");
+        if (Input.GetButtonDown("Jump"))
+            Debug.Log("점프");
+        if (Input.GetButton("Jump"))
+            Debug.Log("점프 누르는중");
+        if (Input.GetButtonUp("Jump"))
+            Debug.Log("점프 뗌");
     }
 }
