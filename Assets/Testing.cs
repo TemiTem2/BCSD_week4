8e2826b77bcafd5aae63ab3b8dbbb89149ff0de8
Mozin <temiumaru@naver.com>
2025-04-01 16:17:20 +0900

    축입력

--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -10,11 +10,16 @@ public class Testing : MonoBehaviour
         if (Input.anyKey)
             Debug.Log("키 누르는 중");
 
-        if (Input.GetButtonDown("Jump"))
-            Debug.Log("점프");
-        if (Input.GetButton("Jump"))
-            Debug.Log("점프 누르는중");
-        if (Input.GetButtonUp("Jump"))
-            Debug.Log("점프 뗌");
+        if (Input.GetButton("Horizontal"))
+        {
+            Debug.Log("횡이동중..."+ Input.GetAxis("Horizontal"));
+            
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            Debug.Log("종이동중..." + Input.GetAxis("Vertical"));
+
+        }
     }
 }
